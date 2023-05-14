7820ccd2fe387f97453ea9620a1d50ed9c0a446d
JakubLisik <116484013+JakubLisik@users.noreply.github.com>
2023-05-14 18:28:46 +0200

    Add files via upload

---
+++ parChecker.py
@@ -0,0 +1,19 @@
+from new import Stack
+
+def parChecker(symbolString):
+    s = Stack()
+    index = 0
+    error = True
+    while index < len(symbolString) and error:
+        symbol = symbolString[index]
+        if symbol=="(": s.push(symbol)
+        else:
+            if s.isEmpty(): error=False
+            else: s.pop()
+        index += 1
+    if error and s.isEmpty() : return True
+    else: return False
+
+print(parChecker('((()))'))
+print(parChecker('((())))))'))
+print(parChecker('(((()()()())))'))
